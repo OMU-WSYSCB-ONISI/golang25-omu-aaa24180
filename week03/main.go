a67e3592abf25a90646207fefd66627a889ff4d9
Shimei Fujii <sg24616g@st.omu.ac.jp>
2025-11-27 11:28:03 +0900

    a

--- week03/main.go
+++ week03/main.go
@@ -1,13 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
 
 func main() {
-	// Week 03: ここに課題のコードを記述してください
-	// 詳細な課題内容はLMSで確認してください
-	
-	fmt.Println("Week 03 課題")
-	
-	// 以下に実装してください
-	
+	http.HandleFunc("/hello", hellohandler)
+	http.HandleFunc("/now", nowhandler)
+	http.HandleFunc("/webfortune", fortunehandler)
+
+	http.ListenAndServe(":8080", nil)
+}
+func hellohandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "こんにちは from Cocespace !")
+}
+func nowhandler(w http.ResponseWriter, r *http.Request) {
+	jst, _ := time.LoadLocation("Asia/Tokyo")
+	fmt.Fprintln(w, (time.Now().In(jst)).Format("2006年01月02日 15:04:05"))
+}
+
+func fortunehandler(w http.ResponseWriter, r *http.Request) {
+	seed := time.Now().UnixNano()
+	d := rand.New(rand.NewSource(seed))
+	var n int32 = d.Int31n(5)
+	var fortune string
+	if n == 0 {
+		fortune = "大吉"
+	} else if n == 1 {
+		fortune = "吉"
+	} else if n == 2 {
+		fortune = "中吉"
+	} else if n == 3 {
+		fortune = "小吉"
+	} else if n == 4 {
+		fortune = "凶"
+	}
+	fmt.Fprint(w, "あなたの今日の運勢は",fortune,"です")
 }
--- week04/main.go
+++ week04/main.go
@@ -1,13 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
 
 func main() {
-	// Week 04: ここに課題のコードを記述してください
-	// 詳細な課題内容はLMSで確認してください
-	
-	fmt.Println("Week 04 課題")
-	
-	// 以下に実装してください
-	
+	http.HandleFunc("/hello", hellohandler)
+	http.HandleFunc("/now", nowhandler)
+	http.HandleFunc("/dice", dicehandler)
+	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/headera", headera)
+	http.HandleFunc("/info", info)
+
+	http.ListenAndServe(":8080", nil)
 }
+func hellohandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "こんにちは from Cocespace !")
+}
+func nowhandler(w http.ResponseWriter, r *http.Request) {
+	jst, _ := time.LoadLocation("Asia/Tokyo")
+	fmt.Fprintln(w, (time.Now().In(jst)).Format("2006年01月02日 15:04:05"))
+}
+func dicehandler(w http.ResponseWriter, r *http.Request) {
+	seed := time.Now().UnixNano()
+	d := rand.New(rand.NewSource(seed))
+	fmt.Fprintln(w, d.Int31n(5)+1)
+}
+func headers(w http.ResponseWriter, r *http.Request) {
+	h := r.Header
+	fmt.Fprintln(w, h)
+}
+func headera(w http.ResponseWriter, r *http.Request) {
+	var h map[string][]string
+	h = r.Header
+	fmt.Fprintln(w, h["Accept-Encoding"][0])
+}
+func info(w http.ResponseWriter, r *http.Request){
+	var h map[string][]string
+	h = r.Header
+	jst, _ := time.LoadLocation("Asia/Tokyo")
+	fmt.Fprintln(w,"現在時刻は、" ,(time.Now().In(jst)).Format("15:04"),"で、使用しているブラウザーは、" ,h["User-Agent"][0],"ですね。")
+}
+
