675d6d2db29ce58e7046c530cbd2a005a08c0701
Shimei Fujii <sg24616g@st.omu.ac.jp>
2025-12-04 05:15:44 +0000

    Pending changes exported from your codespace

--- week08/main.go
+++ week08/main.go
@@ -1,13 +1,166 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	// Week 08: ここに課題のコードを記述してください
-	// 詳細な課題内容はLMSで確認してください
-	
-	fmt.Println("Week 08 課題")
-	
-	// 以下に実装してください
-	
+	fmt.Printf("Go version: %s\n", runtime.Version())
+
+	http.Handle("/", http.FileServer(http.Dir("public/")))
+	http.HandleFunc("/hello", hellohandler)
+	http.HandleFunc("/enq", enqhandler)
+	http.HandleFunc("/fdump", fdump)
+	http.HandleFunc("/cal00", cal00handler)
+	http.HandleFunc("/cal01", calpmhandler)
+	http.HandleFunc("/sum", sumhandler)
+	http.HandleFunc("/bmi", bmicalc)
+	http.HandleFunc("/cal02", calpmhandler2)
+	http.HandleFunc("/ave", avehandler)
+	fmt.Println("Launch server...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Failed to launch server: %v", err)
+	}
+}
+
+func hellohandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "こんにちは from Codespace !")
+}
+
+func fdump(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Println("errorだよ")
+	}
+	// フォームはマップとして利用でき以下で内容を確認できる．
+	for k, v := range r.Form {
+		fmt.Printf("%v : %v\n", k, v)
+	}
+}
+
+func enqhandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Println("errorだよ")
+	}
+	// r.FormValue("name")として，フォーム中name欄の値を得る
+	fmt.Fprintln(w, r.FormValue("name")+"さん，ご協力ありがとうございます.\n年齢は"+r.FormValue("age")+"で，性別は"+r.FormValue("gend")+"で，出身地は"+r.FormValue("birthplace")+"ですね")
+}
+
+func cal00handler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Println("errorだよ")
+	}
+	price, _ := strconv.Atoi(r.FormValue("price"))
+	num, _ := strconv.Atoi(r.FormValue("num"))
+	fmt.Fprint(w, "合計金額は ")
+	fmt.Fprintln(w, price*num)
+}
+
+func calpmhandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Println("errorだよ")
+	}
+	x, _ := strconv.Atoi(r.FormValue("x"))
+	y, _ := strconv.Atoi(r.FormValue("y"))
+	switch r.FormValue("cal0") {
+	case "+":
+		fmt.Fprintln(w, x+y)
+	case "-":
+		fmt.Fprintln(w, x-y)
+	}
+}
+
+func sumhandler(w http.ResponseWriter, r *http.Request) {
+	var sum, tt int
+	if err := r.ParseForm(); err != nil {
+		fmt.Println("errorだよ")
+	}
+	tokuten := strings.Split(r.FormValue("dd"), ",")
+	fmt.Println(tokuten)
+	for i := range tokuten {
+		tt, _ = strconv.Atoi(tokuten[i])
+		sum += tt
+	}
+	fmt.Fprintln(w, sum)
+	fmt.Println(sum)
+}
+func bmicalc(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Println("errorだよ")
+	}
+	weight, _ := strconv.Atoi(r.FormValue("weight"))
+	height, _ := strconv.Atoi(r.FormValue("height"))
+
+	fmt.Fprintln(w, float32(weight)/(float32(height*height)/10000.0))
+
+}
+func calpmhandler2(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Println("errorだよ")
+	}
+	x, _ := strconv.Atoi(r.FormValue("x"))
+	y, _ := strconv.Atoi(r.FormValue("y"))
+	switch r.FormValue("cal2") {
+	case "+":
+		fmt.Fprintln(w, x+y)
+	case "-":
+		fmt.Fprintln(w, x-y)
+	case "*":
+		fmt.Fprintln(w, float32(x)*float32(y))
+	case "/":
+		fmt.Fprintln(w, float32(x)/float32(y))
+	}
+}
+func avehandler(w http.ResponseWriter, r *http.Request) {
+	var sum, tt int
+	var k [10]int = [10]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	if err := r.ParseForm(); err != nil {
+		fmt.Println("errorだよ")
+	}
+	tokuten := strings.Split(r.FormValue("ddd"), ",")
+	fmt.Println(tokuten)
+	for i := range tokuten {
+		tt, _ = strconv.Atoi(tokuten[i])
+		if tt < 10 {
+			k[0]++
+		} else if tt < 20 {
+			k[1]++
+		} else if tt < 30 {
+			k[2]++
+		} else if tt < 40 {
+			k[3]++
+		} else if tt < 50 {
+			k[4]++
+		} else if tt < 60 {
+			k[5]++
+		} else if tt < 70 {
+			k[6]++
+		} else if tt < 80 {
+			k[7]++
+		} else if tt < 90 {
+			k[8]++
+		} else {
+			k[9]++
+		}
+	}
+	sum += tt
+
+	fmt.Fprint(w, "平均：")
+	fmt.Fprintln(w, float32(sum)/float32(len(tokuten)))
+	fmt.Fprintln(w, "")
+
+	fmt.Fprintln(w, 0)
+	var s int
+	for s = 0; s < len(k); s++ {
+		var i int
+		for i = 0; i < k[s]; i++ {
+			fmt.Fprint(w, "*")
+		}
+		fmt.Fprintln(w, "")
+		fmt.Fprintln(w, (s+1)*10)
+	}
+	fmt.Println(sum)
 }
--- week10/main.go
+++ week10/main.go
@@ -1,13 +1,90 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"net/http"
+	"os"
+	"runtime"
+)
+
+const saveFile1 = "public/memo1.txt" // データファイルの保存先
+const saveFile2 = "public/memo2.txt"
+const saveFile3 = "public/memo3.txt"
+
 
 func main() {
-	// Week 10: ここに課題のコードを記述してください
-	// 詳細な課題内容はLMSで確認してください
-	
-	fmt.Println("Week 10 課題")
-	
-	// 以下に実装してください
-	
+	fmt.Printf("Go version: %s\n", runtime.Version())
+
+	http.HandleFunc("/hello", hellohandler)
+	http.HandleFunc("/memo", memo)
+	http.HandleFunc("/mwrite", mwrite)
+
+	fmt.Println("Launch server...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Failed to launch server: %v", err)
+	}
+}
+
+func hellohandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "こんにちは from Codespace !")
+}
+
+func memo(w http.ResponseWriter, r *http.Request) {
+	// データファイルを開く
+	text, err := os.ReadFile(saveFile1)
+	if err != nil {
+		text = []byte("ここにメモを記入してください。")
+	}
+	text1, err := os.ReadFile(saveFile1)
+	if err != nil {
+		text1 = []byte("未記入")
+	}
+	text2, err := os.ReadFile(saveFile2)
+	if err != nil {
+		text2 = []byte("未記入")
+	}
+	text3, err := os.ReadFile(saveFile3)
+	if err != nil {
+		text3 = []byte("未記入")
+	}
+	// HTMLのフォームを返す
+	htmlText := html.EscapeString(string(text))
+	Text1 := html.EscapeString(string(text1))
+	Text2 := html.EscapeString(string(text2))
+	Text3 := html.EscapeString(string(text3))
+	s := "<html>" +
+	"<style>textarea { width:99%; height:200px; }</style>" +
+	"<form method='get' action='/mwrite'>" +
+	"<textarea name='text'>" + htmlText + "</textarea>" +
+	"保存場所："+"<br>"+
+    "<input type='radio' name='wmemo' value='+' checked> memo1" +"<br>"+Text1+"<br>"+
+    "<input type='radio' name='wmemo' value='-' > memo2" +"<br>"+Text2+"<br>"+
+    "<input type='radio' name='wmemo' value='*' > memo3"+"<br>"+Text3+"<br>"+
+	"<input type='submit' value='保存' /></form></html>"
+	w.Write([]byte(s))
+r.ParseForm()
+}
+
+func mwrite(w http.ResponseWriter, r *http.Request) {
+	// 投稿されたフォームを解析
+	r.ParseForm()
+	if len(r.Form["text"]) == 0 { // 値が書き込まれてない時
+		w.Write([]byte("フォームから投稿してください。"))
+		return
+	}
+	text := r.Form["text"][0]
+	// データファイルへ書き込む
+	switch r.Form["wmemo"][0]{
+	case "+":
+		os.WriteFile(saveFile1, []byte(text), 0644)
+	case"-":
+		os.WriteFile(saveFile2, []byte(text), 0644)
+	case "*":
+		os.WriteFile(saveFile3, []byte(text), 0644)
+	}
+
+	fmt.Println("save: " + text)
+	// ルートページへリダイレクトして戻る --- (*4)
+	http.Redirect(w, r, "/memo", 301)
 }
